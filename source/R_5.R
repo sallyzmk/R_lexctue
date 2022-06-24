37d0688422bcf971cf13e58f6a43e3c886eebed0
sallyzmk <sallyzmk@naver.com>
2022-06-24 14:58:58 +0900

    “update”

--- source/R_5.R
+++ source/R_5.R
@@ -1,44 +1,4 @@
-# 데이터 불러오기
-# 경로 설정이 매우 중요
-#
-getwd()
-# setwd("c:/")
-# csv 파일 / 엑셀 파일
-
 # 5장은 스스로 정리
-# 91p mpg1.csv 파일 불러오기
-getwd()
-mpg1 <- read.csv("mpg1.csv") # “” 잊지 말 것!!!
-# mpg1 <- read.csv("mpg1.csv", stringsAsFactors =F) # 문자형을 펙터로 X
-#함수에 다양한 옵션이 있음. 알아만 두기. F를 T로 변경하면 문자형을 펙터로 O
-str(mpg1)  
-
-# 기본 통계 함수
-sum(mtcars$mpg)  # 합
-range(mtcars$mpg)  # 범위
-max(mtcars$mpg)  # 최댓값
-min(mtcars$mpg)  # 최솟값
-quantile(mtcars$mpg)  # 사분위수
-IQR(mtcars$mpg)  # 1~3분위 범위
-
-# 평균
-mean(mtcars$mpg) # 평균
-var(mtcars$mpg) # 분산
-sd(mtcars$mpg) # 표준편차(편차를 제곱_정수로 만들기 위해,그리고 다시 루트)
-# 표준편차는 여러개를 비교해 볼 때 의미가 있다
-# descriptive Statistics : 통계를 묘시히다.
-
-# 수리통계 -> 수학적으로 증명
-# 그러나 일반인은 수학적 증명 할 필요 없음 -> 함수활용, 의미만 알아두기
-sd(mtcars$mpg) 
-
-
-# 사분위수 : 전체 수를 4등분한 수 ex. 분위
-# 사분위수를 기준점으로 잡는다. 분포를 보기 위해. 평균값은 특출난 1인으로 인해 외곡될 수 있음. 그럼 그 특이 케이스인 1인을 무슨 기준으로 데이터에서 뺄 것이냐를 사분위로 결정.
-# IQR : 1사분위수와 3사분위수 사이의 거리
-# 통계분석 / 머닝머신 -> 이상치 제거 할 때 활용
-quantile(mtcars$mpg)
-
 # 통계 요약 구하기 90p
 summary(iris)
 
